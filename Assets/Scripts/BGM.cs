a1c1238a5f550a186086bc15d3f8c7bfff9c38c0
Liangji Ma <15703476199ma@qmail.com>
2020-12-16 14:30:56 -0500

    Liangji Ma: Music

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BGM : MonoBehaviour
+{
+    public AudioClip music;
+    public Slider slider;
+    private AudioSource back;
+    void Start()
+    {
+        back = this.GetComponent<AudioSource>();
+        back.loop = true; 
+        back.volume = 0.5f;
+        back.clip = music;
+        back.Play(); 
+    }
+
+    void Update()
+    {
+        back.volume = slider.value;
+    }
+}
